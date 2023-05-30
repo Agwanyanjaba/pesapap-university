70526828694beb72f166c368d57cf03b99d2b669
Agwanyanjaba <bonkewycliffe@yahoo.com>
2023-05-30 07:37:35 +0300

    configure server port

---
+++ src/main/java/com/pesapauniversity/PesapauniversityApplication.java
@@ -0,0 +1,13 @@
+package com.pesapauniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesapauniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesapauniversityApplication.class, args);
+	}
+
+}
